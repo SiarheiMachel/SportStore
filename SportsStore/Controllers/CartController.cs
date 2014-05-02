dc16c48608b49a72291299fe7be27a6cc32458bc
Sergey1209 <sergio177@yandex.ru>
2014-05-02 23:12:07 +0300

    sdfgdf

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -3,18 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.DataAccess.Repositories;
+using SportsStore.Web.Models;
 
 namespace SportsStore.Web.Controllers
 {
     public class CartController : Controller
     {
-        //
-        // GET: /Cart/
+        private readonly IProductRepository _productRepository;
 
-        public ActionResult Index()
+        public CartController(IProductRepository productRepository)
         {
-            return View();
+            _productRepository = productRepository;
         }
 
+        public Cart Cart
+        {
+            get
+            {
+                var cartInSession = (Cart)Session["Cart"];
+
+                if (cartInSession != null) return cartInSession;
+                var cart = new Cart();
+                Session["Cart"] = cart;
+                return cart;
+            }
+        }
+
+        public RedirectToRouteResult AddToCart(int id, string returnUrl)
+        {
+            var item = _productRepository.GetAllProducts().FirstOrDefault(t => t.Id == id);
+
+            if (item != null)
+            {
+                Cart.Add(item, 1);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int id, string returnUrl)
+        {
+            var item = _productRepository.GetAllProducts().FirstOrDefault(t => t.Id == id);
+
+            if (item != null)
+            {
+                Cart.Remove(item);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
     }
 }
